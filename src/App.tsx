4d8af3494520eb7f17481d5771ac66f5def2d776
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-09-12 08:57:26 +1200

    improve ui

--- src/App.tsx
+++ src/App.tsx
@@ -31,48 +31,50 @@ export function App() {
           </div>
           <Input type="file" />
         </div>
-        <Card id="parameters">
-          <CardHeader>
-            <CardTitle>Cache size</CardTitle>
-            <CardDescription>
-              <p>
-                S = 2<sup>s</sup>
-              </p>
-              <p>E: number of cache lines</p>
-              <p>
-                B = 2<sup>b</sup>
-              </p>
-              <div>
-                <pre>
-                  <code>C = S * E * B = 16 bytes</code>
-                </pre>
+        <div className="grid grid-cols-2 gap-4">
+          <Card id="parameters">
+            <CardHeader>
+              <CardTitle>Cache size</CardTitle>
+              <CardDescription>
+                <p>
+                  S = 2<sup>s</sup>
+                </p>
+                <p>E: number of cache lines</p>
+                <p>
+                  B = 2<sup>b</sup>
+                </p>
+                <div>
+                  <pre>
+                    <code>C = S * E * B = 16 bytes</code>
+                  </pre>
+                </div>
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="flex flex-col gap-y-1">
+                <div className="flex justify-between">
+                  <label>s</label>
+                  <p>4</p>
+                </div>
+                <Slider step={1} min={1} max={10} defaultValue={[4]} />
               </div>
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="flex flex-col gap-y-1">
-              <div className="flex justify-between">
-                <label>s</label>
-                <p>4</p>
+              <div className="flex flex-col gap-y-1">
+                <div className="flex justify-between">
+                  <label>E</label>
+                  <p>1</p>
+                </div>
+                <Slider step={1} min={1} max={10} defaultValue={[1]} />
               </div>
-              <Slider step={1} min={1} max={10} defaultValue={[4]} />
-            </div>
-            <div className="flex flex-col gap-y-1">
-              <div className="flex justify-between">
-                <label>E</label>
-                <p>1</p>
+              <div className="flex flex-col gap-y-1">
+                <div className="flex justify-between">
+                  <label>b</label>
+                  <p>4</p>
+                </div>
+                <Slider step={1} min={1} max={10} defaultValue={[4]} />
               </div>
-              <Slider step={1} min={1} max={10} defaultValue={[1]} />
-            </div>
-            <div className="flex flex-col gap-y-1">
-              <div className="flex justify-between">
-                <label>b</label>
-                <p>4</p>
-              </div>
-              <Slider step={1} min={1} max={10} defaultValue={[4]} />
-            </div>
-          </CardContent>
-        </Card>
+            </CardContent>
+          </Card>
+        </div>
         <div className="grid grid-cols-2 gap-4">
           <div id="controls" className="space-x-4 flex justify-end">
             <Button variant="default" className="space-x-2">
@@ -83,7 +85,7 @@ export function App() {
             </Button>
           </div>
 
-          <div id="addresses" className="col-start-1">
+          <div id="addresses" className="col-start-1 overflow-auto h-48">
             <Table>
               <TableHeader>
                 <TableRow>
@@ -91,17 +93,95 @@ export function App() {
                   <TableHead>Inst.</TableHead>
                   <TableHead>Address</TableHead>
                   <TableHead>Tag</TableHead>
-                  <TableHead>SI</TableHead>
+                  <TableHead>Set Index</TableHead>
                   <TableHead>B</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
                 <TableRow>
-                  <TableCell className="text-zinc-600 size-8"> →</TableCell>
-                  <TableCell className="text-zinc-600"> L</TableCell>
-                  <TableCell className="text-zinc-600 text-right">
-                    0x54C
-                  </TableCell>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
+                  <TableCell className="text-amber-600">0000</TableCell>
+                  <TableCell className="text-green-600">1000</TableCell>
+                  <TableCell className="text-zinc-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-foreground size-8"> →</TableCell>
+                  <TableCell> L</TableCell>
+                  <TableCell className="text-right">0x54C</TableCell>
                   <TableCell className="text-amber-600">0000</TableCell>
                   <TableCell className="text-green-600">1000</TableCell>
                   <TableCell className="text-zinc-600">1111</TableCell>
@@ -109,22 +189,73 @@ export function App() {
               </TableBody>
             </Table>
           </div>
-          <div id="sets" className="space-y-2 ">
-            <Card id="set">
-              <CardContent className="p-4 flex items-center gap-x-4">
-                <p>
-                  Set <span className="text-green-600">0</span>
-                </p>
-                <div className="flex gap-x-4 border border-zinc-300 rounded p-2">
-                  <div id="valid" className="text-pink-600">
-                    1
-                  </div>
-                  <div id="tag" className="text-amber-600">
-                    01000
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
+          <div id="sets" className="space-y-2 overflow-auto h-48">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-20">Set Index</TableHead>
+                  <TableHead className="w-10">Valid</TableHead>
+                  <TableHead>Tag</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className="text-green-600">0000</TableCell>
+                  <TableCell className="text-pink-600">1</TableCell>
+                  <TableCell className="text-amber-600">1111</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
           </div>
         </div>
       </div>
